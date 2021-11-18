1088fc0b60d41de63354cd374a69df21af790bf9
keithwillis-bc <kwillis@beckman.com>
2021-11-18 16:51:02 -0500

    added ExpressJs

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
-const username = "Keith";
+const express = require("express");
 
-console.log(username);
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/currenttime", (req, res) => {
+  console.log("called currenttime");
+
+  res.send("<h1>" + new Date().toISOString() + "</h1>");
+});
+
+app.get("/", (req, res) => {
+  console.log("called root path");
+
+  res.send(
+    "<form method='POST' action='/store-user'><label>Your Name: </label><input name='username' type='text'><button type='submit'>Submit</button></form>"
+  );
+});
+
+app.post("/store-user", (req, res) => {
+  const userName = req.body.username;
+  console.log(userName);
+
+  res.send("<h1>UserName stored</h1>");
+});
+
+app.listen(3000);
